7637f0de7013b2cfecfb84574b7b3d4269f06c0e
hariseldon2073 <hariseldon2073@gmail.com>
2021-02-13 08:19:57 +0100

    Added my prj

---
+++ src/app/app.component.ts
@@ -0,0 +1,73 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+    if(!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }  
+  }  
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick() {
+    console.log(
+      `About to generate a password with the following:
+      Include letters: ${this.includeLetters}
+      Include numbers: ${this.includeNumbers}
+      Include symbols: ${this.includeSymbols}
+    `)
+    //this.password = 'MY PASSWORD!!!';
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if(this.includeLetters) {
+      validChars += letters;
+    }
+    if(this.includeNumbers) {
+      validChars += numbers;
+    }
+    if(this.includeSymbols){
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for(let i = 0; i < this.length; i++){
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
+  }
+
+  getPassword() {
+    return this.password;
+  }
+
+  getName() {
+    return 'Marco';
+  }
+
+} 
